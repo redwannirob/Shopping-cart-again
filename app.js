badad25e7f24736030a864b67f89d2f4276d5455
Redwan Nirob <Redwan Nirob>
2021-09-04 22:34:58 +0600

    added a function

--- app.js
+++ app.js
@@ -17,26 +17,69 @@ function updateProductsNumber(product,price,isIncreasing){
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = productNumber * price;
 
+    calculateTotal('phone','case');
+
 };
 
+function getInputValue(product){
+    const productInput = document.getElementById(product,'-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
 
+function calculateTotal(productId){
+    const phoneTotal = document.getElementById('phone-total');
+    const phoneTotalAmount = parseInt(phoneTotal.innerText)
+
+    const caseTotal = document.getElementById('case-total');
+    const caseTotalAmount = parseInt(caseTotal.innerText);
+
+    const subTotal = document.getElementById('sub-total');
+    subTotal.innerText = phoneTotalAmount + caseTotalAmount;
+    console.log(subTotal.innerText);
+    // const phoneTotal = getInputValue('phone') * 1219;
+    // const caseTotal = getInputValue('case') * 59;
+
+    // console.log(phoneTotal);
+
+    // // const subTotal = phoneTotal + caseTotal;
+    // // document.getElementById('sub-total').innerText = subTotal;
+
+    
+};
+
+function calculateTax(){
+    const subTotal = document.getElementById('sub-total');
+    const subTotalAmount = parseInt(subTotal.innerText);
+    const totalPrice = document.getElementById('total-price');
+    // const totalPriceNumber = parseInt(totalPrice.innerText);
+    const taxAmount = document.getElementById('tax-amount');
+    const taxNumber = parseInt(taxAmount.innerText);
+    taxAmount.innerText = subTotalAmount * 0.05;
+    totalPrice.innerText = subTotalAmount + parseInt(taxAmount.innerText) * 0.05;
+    // totalPrice.innerText = 
+};
 
 const phoneIncreaseBtn = document.getElementById('phone-plus');
 phoneIncreaseBtn.addEventListener('click',function(){
     updateProductsNumber('phone',1219,true);
+    calculateTax()
 });
 
 const phoneDecreaseBtn = document.getElementById('phone-minus');
 phoneDecreaseBtn.addEventListener('click',function(){
     updateProductsNumber('phone',1219,false);
+    calculateTax();
 });
 
 const caseIncreaseBtn = document.getElementById('case-plus');
 caseIncreaseBtn.addEventListener('click',function(){
     updateProductsNumber('case',59,true);
+    calculateTax();
 });
 
 const caseDecreaseBtn = document.getElementById('case-minus');
 caseDecreaseBtn.addEventListener('click',function(){
     updateProductsNumber('case',59,false);  
+    calculateTax();
 });
